eed7947f37b9ccfbf47898063398013f7686205c
solomonleke <moyinadeleke@yahoo.com>
2022-02-18 17:16:49 +0100

    update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -161,7 +161,7 @@ class AdminController extends Controller
     }
     public function calculator(Request $request)
     {
-        $rates = Calculator::find(1); //in array
+        $rates = Calculator::find(7); //in array
 
        return view('Admin.calculator', compact('rates'));
     }
